e87bda4976efa812ec3889cb8ed488c38abd4422
cpraveen <cpraveen@gmail.com>
2015-02-21 12:11:38 +0530

    cheb function has been simplified

--- chebPy.py
+++ chebPy.py
@@ -1,4 +1,4 @@
-from numpy import pi,cos,arange,ones,tile,dot,eye,diag,linspace
+from numpy import pi,cos,arange,ones,tile,dot,eye,diag
 
 def cheb(N):
     '''Chebushev polynomial differentiation matrix.
@@ -7,13 +7,11 @@ def cheb(N):
     x      = cos(pi*arange(0,N+1)/N)
     if N%2 == 0:
         x[N/2] = 0.0 # only when N is even!
-    c      = ones(N+1)
-    c[0]   = 2.0
-    c[N]   = 2.0
+    c      = ones(N+1); c[0] = 2.0; c[N] = 2.0
     c      = c * (-1.0)**arange(0,N+1)
+    c      = c.reshape(N+1,1)
     X      = tile(x.reshape(N+1,1), (1,N+1))
     dX     = X - X.T
-    D      = dot(c.reshape(N+1,1),(1.0/c).reshape(1,N+1))
-    D      = D / (dX+eye(N+1))
-    D      = D - diag( D.T.sum(axis=0) )
+    D      = dot(c, 1.0/c.T) / (dX+eye(N+1))
+    D      = D - diag( D.sum(axis=1) )
     return D,x
